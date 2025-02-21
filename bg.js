04c95952eac05461379f965c3f1f3fd2e3751962
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-21 17:11:31 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,4 +1,4 @@
-chrome.contentSettings.javascript.get({ primaryUrl: "https://ahonoora.com" }, async e => (
+chrome.contentSettings.javascript.get({ primaryUrl: "https://ahonoora.com" }, e => (
   e.setting == "allow" && chrome.contentSettings.javascript.set({
     primaryPattern: "https://ahonoora.com/*",
     setting: "block"
