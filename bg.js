9cafd271c56458e4c6ec5842aa082b14b1d62c01
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-24 19:26:47 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
-chrome.contentSettings.javascript.get({ primaryUrl: "https://ahonoora.com" }, e => (
+chrome.contentSettings.javascript.get({ primaryUrl: "https://ahonoora.com" }, e =>
   e.setting == "allow" && chrome.contentSettings.javascript.set({
     primaryPattern: "https://ahonoora.com/*",
     setting: "block"
   })
-));
+);
