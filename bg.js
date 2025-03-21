7cfd786e1a5578d074a0fec3f011738d19675f0e
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-21 15:57:04 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,8 +1,14 @@
-chrome.runtime.onInstalled.addListener(() =>
+chrome.runtime.onInstalled.addListener(() => (
   chrome.contentSettings.javascript.get({ primaryUrl: "https://ahonoora.com" }, details =>
     details.setting == "allow" && chrome.contentSettings.javascript.set({
       primaryPattern: "https://ahonoora.com/*",
       setting: "block"
     })
+  ),
+  chrome.contentSettings.images.get({ primaryUrl: "https://ahonoora.com" }, details =>
+    details.setting == "allow" && chrome.contentSettings.images.set({
+      primaryPattern: "https://ahonoora.com/*",
+      setting: "block"
+    })
   )
-);
+));
