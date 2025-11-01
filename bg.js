51cdc645b4d8f80d4a5767c444f90f6f63b922db
a <106607480+ariamaranai@users.noreply.github.com>
2025-11-02 02:31:55 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,9 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
+  let { images, javascript } = chrome.contentSettings;
   let primaryUrl = { primaryUrl: "https://ahonoora.com" };
   let value = { primaryPattern: "https://ahonoora.com/*", setting: "block" };
-  let contentSettings = chrome.contentSettings;
-  let javascript = contentSettings.javascript;
-  javascript.get(primaryUrl, details => details.setting == "allow" && javascript.set(value));
-  let images = contentSettings.images;
   images.get(primaryUrl, details => details.setting == "allow" && images.set(value));
+  javascript.get(primaryUrl, details => details.setting == "allow" && javascript.set(value));
 });
